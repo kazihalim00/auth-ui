2ea76869928acd6ba5b32439d1ccf9e0cbbef671
Kazi Abdul Halim <kazihalim44@gmail.com>
2026-03-30 20:37:38 +0600

    Title and Subtitle Added

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,47 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body:Center(
-          child: Text(
-            'Initial Version',
-            style: TextStyle(fontSize: 20),
-          ),
-        )
-      )
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget{
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 60),
+
+                const Text(
+                  'Welcome Back',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 8),
+
+                const Text(
+                  'Enter your credential to login',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          )
+      ),
     );
   }
 
